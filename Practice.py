cc643bbaf2bd34c6a791092b0f521d1c0e968907
ahmedgit2024 <ahmedaslam.07@gmail.com>
2024-12-01 13:58:09 +0530

    Practice

---
+++ Practice.py
@@ -0,0 +1,25 @@
+# 1.  Python Program to add two integer values.
+
+a = 15
+b = 12
+
+print(a+b)
+
+# 2. Python Program to subtract two integer values.
+
+print(a-b)
+
+# 3). Python program to multiply two numbers.
+
+print(a*b)
+
+# 4). Python program to repeat a given string 5 times.
+
+# Input :
+str1 = "SQATools"
+print(str1*5)
+
+# 5).Python program to get the Average of given numbers.
+c=11
+print((a+b+c)/3)
+
